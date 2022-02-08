73b7b5bdc3fdb4576dd6fe8099790ac5d8281d30
Cube <46578659+cnuebred@users.noreply.github.com>
2022-02-08 17:37:06 +0100

    update main sample

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { randomBytes } from 'crypto'
-import { QRcode as Wr } from './qr_code'
+import { QRcode } from './qr_code'
 
 const app = async () => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const data = {
         time: Date.now(),
         message: 'Hello world',
         next: randomBytes(16).toString('hex')
     }
-    new Wr('Hello World', { minErrorLevel: 'M', minVersion: 1 }).render()
+    new QRcode(JSON.stringify(data), { minErrorLevel: 'M' }).render()
 }
+
+
 app()
 
 
